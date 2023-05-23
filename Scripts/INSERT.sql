01c10c7c42d83c5e152fc1693f5e9b159108312d
EvgeniiNoName <125493179+EvgeniiNoName@users.noreply.github.com>
2023-05-23 20:35:02 +0700

    Fixed remarks

--- Scripts/INSERT.sql
+++ Scripts/INSERT.sql
@@ -8,10 +8,21 @@ INSERT INTO Tracks(Duration, Name_Track, Album_id)
 	VALUES
 	('190', 'Первая', 1),
 	('200', 'Мой Второй', 1),
-	('139', 'Четвертая', 2),
-	('179', 'Пятая', 2),
-	('300', 'Седьмая', 3),
-	('182', 'Восьмая', 3);
+	('139', 'Четвертая', 1),
+	('179', 'Пятая', 1),
+	('300', 'Седьмая', 1),
+	('300', 'my own', 1),
+	('300', 'own my', 2),
+	('300', 'my', 2),
+	('300', 'oh my god', 2),
+	('300', 'myself', 2),
+	('300', 'by myself', 2),
+	('300', 'bemy self', 3),
+	('300', 'go my', 3),
+	('300', 'myself by', 3),
+	('300', 'by myself by', 3),
+	('300', 'beemy', 3),
+	('182', 'premyne', 3);
 	
 INSERT INTO Genres(Name_Genres) 
 	VALUES
@@ -39,22 +50,36 @@ INSERT INTO TrackCollection(Track_id_, Collection_id_)
 	(2, 2),
 	(3, 3),
 	(4, 4),
-	(5, 4),
-	(6, 2);
+	(5, 1),
+	(6, 2),
+	(7, 3),
+	(8, 4),
+	(9, 1),
+	(10, 2),
+	(11, 3),
+	(12, 4),
+	(13, 1),
+	(14, 2),
+	(15, 3),
+	(16, 4),
+	(17, 1);
 
 INSERT INTO AlbumPerformer(Performer_id_, Album_id_)
 	VALUES
 	(1, 1),
 	(2, 2),
 	(3, 3),
+	(1, 2),
+	(2, 1),
 	(4, 1);
 
 INSERT INTO GenrePerformer(Performer_id_, Genre_id_)
 	VALUES
 	(1, 1),
 	(2, 2),
-	(3, 3),
+	(3, 2),
 	(4, 2);
 
 
 TRUNCATE albums, Tracks, Genres, Performers, Collections, TrackCollection RESTART IDENTITY CASCADE;
+TRUNCATE AlbumPerformer RESTART IDENTITY CASCADE;
--- Scripts/SELECT.sql
+++ Scripts/SELECT.sql
@@ -11,10 +11,10 @@ SELECT Name_Performers FROM Performers
 WHERE (char_length(Name_Performers) - char_length(REPLACE(Name_Performers, ' ',''))) = 0;
 
 SELECT Name_Track FROM Tracks
-WHERE upper(Name_Track) LIKE '%МОЙ%' OR upper(Name_Track) LIKE '%MY%';
+WHERE Name_Track ~* '\yМОЙ\y' OR Name_Track ~* '\yMY\y';
 
 SELECT Name_Genres, COUNT(Performer_id_) FROM GenrePerformer
-JOIN Genres ON GenrePerformer.Genre_id_=Genres.Genre_id
+RIGHT JOIN Genres ON GenrePerformer.Genre_id_=Genres.Genre_id
 GROUP BY Name_Genres;
 
 SELECT count(Name_Track) FROM albums
@@ -25,11 +25,12 @@ SELECT Name_album, AVG(Duration) FROM albums
 JOIN Tracks ON albums.Album_id=Tracks.Album_id
 GROUP BY Name_album;
 
-SELECT Name_Performers FROM AlbumPerformer
-JOIN albums ON AlbumPerformer.Album_id_=Albums.Album_id
-JOIN Performers ON AlbumPerformer.Performer_id_=Performers.Performer_id
-WHERE Year_album != '2020'
-GROUP BY Name_Performers;
+SELECT Name_Performers FROM Performers
+WHERE Name_Performers NOT IN 
+	(SELECT Name_Performers FROM AlbumPerformer
+	JOIN albums ON AlbumPerformer.Album_id_=Albums.Album_id
+	JOIN Performers ON AlbumPerformer.Performer_id_=Performers.Performer_id
+	WHERE Year_album = '2020');
 
 SELECT Name_Collection FROM TrackCollection
 JOIN Tracks ON TrackCollection.Track_id_=Tracks.track_id
